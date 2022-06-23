ac3a3e127634a1ca61e9c6cebae3bab70d448282
majawsb <107943967+191894@users.noreply.github.com>
2022-06-23 12:44:04 +0200

    trzecia zmiana

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string imie;
+    int wiek;
+    cout << "Hej! Podaj swoje imie:" << endl;
+    cin >> imie;
+    cin.ignore();
+    cout << "Podaj swoj wiek:" << endl;
+    cin >> wiek;
+    cin.ignore();
+    cout << "Masz na imie " <<imie<< " i masz " <<wiek<< " lat." << endl;
+
     return 0;
 }
